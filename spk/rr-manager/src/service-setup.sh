c3db2fbf23ba0e87aa1a20680c22c69e9bb5a0fe
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-01-22 16:09:24 +0100

    add pip install post deployment

--- spk/rr-manager/src/service-setup.sh
+++ spk/rr-manager/src/service-setup.sh
@@ -2,6 +2,7 @@
 service_postinst ()
 {
     cp ${SYNOPKG_PKGDEST}/bin/test.cgi ${SYNOPKG_PKGDEST}/app/
-
+    echo "Install packages for RR Manager"
+    pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
     /bin/sqlite3 ${SYNOPKG_PKGHOME}/api.db <${SYNOPKG_PKGDEST}/app/createsqlitedata.sql
 }

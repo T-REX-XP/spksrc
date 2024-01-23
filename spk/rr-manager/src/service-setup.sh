2269602dafebbbec37f862e4ddd34e0488201f41
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-01-23 23:07:02 +0100

    fix

--- spk/rr-manager/src/service-setup.sh
+++ spk/rr-manager/src/service-setup.sh
@@ -1,6 +1,13 @@
+PYTHON_DIR="/usr/local/python3"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
+VIRTUALENV="${PYTHON_DIR}/bin/python3 -m venv"
+PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
 
 service_postinst ()
 {
+     # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
+
     cp ${SYNOPKG_PKGDEST}/bin/test.cgi ${SYNOPKG_PKGDEST}/app/
     echo "Install packages for RR Manager"
     pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt

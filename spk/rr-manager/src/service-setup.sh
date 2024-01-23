6e33567e52c64319a28eaf8b80c9c9d19bfe4ab0
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-01-24 00:08:20 +0100

    fix

--- spk/rr-manager/src/service-setup.sh
+++ spk/rr-manager/src/service-setup.sh
@@ -1,13 +1,20 @@
-PYTHON_DIR="/usr/local/python3"
-PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
-VIRTUALENV="${PYTHON_DIR}/bin/python3 -m venv"
-PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
+# Add python to path
+# This gives tranmission the power to execute python scripts on completion (like TorrentToMedia).
+if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
+    # use system python for DSM7
+    PYTHON_BIN_PATHS=""
+else
+    GROUP="sc-download"
+    PYTHON_BIN_PATHS="/var/packages/python310/target/bin:/var/packages/python38/target/bin:/var/packages/python3/target/bin:"
+fi
+
+PATH="${SYNOPKG_PKGDEST}/bin:${PYTHON_BIN_PATHS}${PATH}"
 
 service_postinst ()
 {
      # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
-
+    python3 -m venv --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
+    python3 -m pip install --upgrade pip
     cp ${SYNOPKG_PKGDEST}/bin/test.cgi ${SYNOPKG_PKGDEST}/app/
     echo "Install packages for RR Manager"
     pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt

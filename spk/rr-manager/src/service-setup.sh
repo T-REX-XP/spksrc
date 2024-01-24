16fe21ab725803209191362739aad8e6584a7329
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-01-24 01:14:26 +0100

    fix

--- spk/rr-manager/src/service-setup.sh
+++ spk/rr-manager/src/service-setup.sh
@@ -20,6 +20,7 @@ service_postinst ()
 
     cp ${SYNOPKG_PKGDEST}/bin/test.cgi ${SYNOPKG_PKGDEST}/app/
     echo "Install packages for RR Manager"
+    python3 -m pip install --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
     pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
     /bin/sqlite3 ${SYNOPKG_PKGHOME}/api.db <${SYNOPKG_PKGDEST}/app/createsqlitedata.sql
 }

ec05b34d6dc586157b7085078d5f8c61c8a9e844
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-03-02 16:29:14 +0100

    Clean up. Add required tasks via the installer

--- spk/rr-manager/src/app/createsqlitedata.sql
+++ spk/rr-manager/src/app/createsqlitedata.sql
@@ -1,6 +1,4 @@
-CREATE TABLE magazines (identifier text, title text, description text);
-
-INSERT INTO magazines VALUES ('1','Wired','Geek magazine');
-INSERT INTO magazines VALUES ('2','Elle','Mode magazine');
-INSERT INTO magazines VALUES ('3','Green','Eco magazine');
+INSERT INTO task VALUES('RunRrUpdate', '', 'bootup', '', 0, 0, 0, 0, '', 0, '/usr/bin/rr-update.sh updateRR /volume1/downloads/tmp_rr_update/update.zip /tmp/rr_update_progress', 'script', '{}', '', '', '{}', '{}');
+INSERT INTO task VALUES('SetRootPrivsToRrManager', '', 'bootup', '', 0, 0, 0, 0, '', 0, 'sed -i 's/package/root/g' /var/packages/rr-manager/conf/privilege
+synopkg restart rr-manager', 'script', '{}', '', '', '{}', '{}');
 
